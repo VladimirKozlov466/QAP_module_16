4bffe0f45c6830ef842c82ace1a1e03c14441206
VladimirKozlov466 <86114164+VladimirKozlov466@users.noreply.github.com>
2021-08-27 17:43:43 +0500

    Update test_16_9_1.py
    my own updates

--- test_16_9_1.py
+++ test_16_9_1.py
@@ -1,35 +1,106 @@
+# import math Неправильный вариант))
+# 
+# 
+# class Figures:
+#     def __init__(self, a, b, c, d, r):
+#         pass
+# 
+# 
+# class Square(Figures):
+#     def __init__(self, a):
+#         self.a = a
+# 
+#     def getArea_square(self):
+#         return self.a ** 2
+# 
+#     def get_square_info(self):
+#         return 'Square ({0})'.format(self.a)
+# 
+# 
+# class Circle(Figures):
+#     def __init__(self, r):
+#         self.r = r
+# 
+#     def getArea_cicle(self):
+#         return math.pi * self.r ** 2
+# 
+#     def get_circle_info(self):
+#         return 'Circle ({0})'.format(self.r)
+# 
+# 
+# class Rectangle(Figures):
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+# 
+#     def getArea_rectangle(self):
+#         return self.a * self.b
+# 
+#     def get_rectangle_info(self):
+#         return 'Rectangle ({0}, {1})'.format(self.a, self.b)
+# 
+# 
+# class Triangle(Figures):
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+# 
+#     def getArea_triangle(self):
+#         p = (self.a + self.b + self.c)
+#         return (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
+# 
+#     def get_triangle_info(self):
+#         return 'Triangle ({0}, {1}, {2})'.format(self.a, self.b, self.c)
+# 
+# 
+# Square = Square(5)
+# print(Square.get_square_info())
+# 
+# Circle = Circle(5)
+# print(Circle.get_circle_info())
+# 
+# Rectangle = Rectangle(5, 10)
+# print(Rectangle.get_rectangle_info())
+# 
+# Triangle = Triangle(5, 6, 7)
+# print(Triangle.get_triangle_info())
+# # ___________________________ Правильный)
 import math
 
 
 class Figures:
-    def __init__(self, a, b, c, d, r):
-        pass
+    def __init__(self, name=None):
+        self.name = name
 
 
 class Square(Figures):
-    def __init__(self, a):
+    def __init__(self, name, a):
+        super().__init__(name)
         self.a = a
 
     def getArea_square(self):
         return self.a ** 2
 
     def get_square_info(self):
-        return 'Square ({0})'.format(self.a)
+        return '{0} ({1})'.format(self.name, self.a)
 
 
 class Circle(Figures):
-    def __init__(self, r):
+    def __init__(self, name, r):
+        super().__init__(name)
         self.r = r
 
     def getArea_cicle(self):
         return math.pi * self.r ** 2
 
     def get_circle_info(self):
-        return 'Circle ({0})'.format(self.r)
+        return '{0} ({1})'.format(self.name, self.r)
 
 
 class Rectangle(Figures):
-    def __init__(self, a, b):
+    def __init__(self,name, a, b):
+        super().__init__(name)
         self.a = a
         self.b = b
 
@@ -37,11 +108,12 @@ class Rectangle(Figures):
         return self.a * self.b
 
     def get_rectangle_info(self):
-        return 'Rectangle ({0}, {1})'.format(self.a, self.b)
+        return '{0} ({1}, {2})'.format(self.name, self.a, self.b)
 
 
 class Triangle(Figures):
-    def __init__(self, a, b, c):
+    def __init__(self, name, a, b, c):
+        super().__init__(name)
         self.a = a
         self.b = b
         self.c = c
@@ -51,17 +123,18 @@ class Triangle(Figures):
         return (p * (p - self.a) * (p - self.b) * (p - self.c)) ** 0.5
 
     def get_triangle_info(self):
-        return 'Triangle ({0}, {1}, {2})'.format(self.a, self.b, self.c)
+        return '{0} ({1}, {2}, {3})'.format(self.name, self.a, self.b, self.c)
 
 
-Square = Square(5)
+Square = Square("Square", 5)
 print(Square.get_square_info())
 
-Circle = Circle(5)
+Circle = Circle("Circle", 5)
 print(Circle.get_circle_info())
 
-Rectangle = Rectangle(5, 10)
+Rectangle = Rectangle("Rectangle", 5, 10)
 print(Rectangle.get_rectangle_info())
 
-Triangle = Triangle(5, 6, 7)
+Triangle = Triangle("Triangle", 5, 6, 7)
 print(Triangle.get_triangle_info())
+
